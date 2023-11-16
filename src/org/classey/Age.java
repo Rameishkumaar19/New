45adf0b7772f9d312aa42d2d71fb7ce48c6a31f3
Rameishkumaar19 <kumarramez19@gmail.com>
2023-11-16 12:29:35 +0530

    yes

---
+++ src/org/classey/Age.java
@@ -0,0 +1,31 @@
+package org.classey;
+
+public class Age {
+
+	 public static void main(String[] args) {
+		
+		 int carspd=400, bikespd=200;
+		 
+		 if (carspd>=400) {
+			System.out.println("carspd is higher than bike spd");
+			
+			if (bikespd>200) {
+				System.out.println("bikespd>carspd");
+				 
+			} else {
+                System.out.println("both are not equal");
+				
+			}
+			
+			
+			
+			} else {
+             System.out.println("both are equal");
+				
+		}
+		  
+		  
+	}
+		
+	
+}
